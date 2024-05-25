d1849f6613880e038c0d3ba405d0563bc4b132c4
Bertrand2808 <bertrand2808@icloud.com>
2024-05-25 14:43:43 +0200

    first commit

---
+++ src/test/java/fr/esgi/Authentification/AuthentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.Authentification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
